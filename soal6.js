487b95ccefb1037b72158587c1abe8db73e5a51b
Geraldo Sepdwijaya <sepdwijaya18@gmail.com>
2020-06-11 11:27:12 +0700

    update soal6.js

--- soal6.js
+++ soal6.js
@@ -1,19 +1,27 @@
 let number1 = 25;
 let number2 = 10;
 result1 = number1 + number2;
+addition = "Addtion: 25 + 10";
+console.log(addition);
 console.log(result1);
 
 let number3 = 100;
 let number4 = 2;
 result2 = number3 * number4;
+multiple = "multiple: 100 * 2";
+console.log(multiple);
 console.log(result2);
 
 let number5 = 99;
 let number6 = 2;
 result3 = number5 / number6;
+division = "Division: 99 / 2";
+console.log(division);
 console.log(result3);
 
 let number7 = 99;
 let number8 = 2;
 result4 = number7 % number8;
+modulus = "Modulus: 99 % 2";
+console.log(modulus);
 console.log(result4);
